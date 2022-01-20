90977d3411d726e21b5ac9ad86d68fef03d17597
ajay-gov <ajay.gov@protonmail.com>
2022-01-19 22:38:56 -0800

    Add Spatial Lane and use it to compute neighbors for a given intersection

--- server/src/main/java/swim/traffic/agent/CityAgent.java
+++ server/src/main/java/swim/traffic/agent/CityAgent.java
@@ -19,15 +19,63 @@ import swim.api.SwimResident;
 import swim.api.agent.AbstractAgent;
 import swim.api.downlink.MapDownlink;
 import swim.api.lane.JoinValueLane;
+import swim.api.lane.SpatialLane;
+import swim.collections.HashTrieMap;
+import swim.math.CircleR2;
+import swim.math.PointR2;
+import swim.math.R2Shape;
+import swim.spatial.SpatialMap;
+import swim.structure.Form;
 import swim.structure.Value;
 import swim.uri.Uri;
 
+import java.util.Iterator;
+
 public class CityAgent extends AbstractAgent {
+
+  @SwimLane("city/map")
+  public SpatialLane<Uri, R2Shape, Value> cityMap = geospatialLane()
+      .keyForm(Uri.form())
+      .valueForm(Form.forValue());
+
   MapDownlink<Uri, Value> intersectionsLink;
 
   @SwimLane("intersections")
   @SwimResident
-  public JoinValueLane<Uri, Value> intersections;
+  public JoinValueLane<Uri, Value> intersections = joinValueLane()
+      .keyForm(Uri.form())
+      .valueForm(Form.forValue())
+      .didUpdate((key, newValue, oldValue) -> computeNeighbors(key, newValue, oldValue));
+
+  void computeNeighbors(Uri intersectionUri, Value newInfo, Value oldInfo) {
+    if (newInfo.get("lng").isDefined() && newInfo.get("lat").isDefined()) {
+      final double newLng = newInfo.get("lng").doubleValue();
+      final double newLat = newInfo.get("lat").doubleValue();
+      // Add to the spatial lane by creating an R2Point with the given lat/lng
+      cityMap.put(intersectionUri, new PointR2(newLng, newLat), newInfo);
+
+      // Get the snapshot from the spatial lane
+      final SpatialMap<Uri, R2Shape, Value> cityMapSnapshot = cityMap.snapshot();
+
+      HashTrieMap<Uri, Value> newIntersections = HashTrieMap.empty();
+      if (!Double.isNaN(newLng) && !Double.isNaN(newLat)) {
+        // create an R2Shape- a Circle in the case, with the lat/lng as the center and 0.0025 as radius (0.0025 equates ~.25-.27 km)
+        final CircleR2 newCircle = new CircleR2(newLng, newLat, 0.0025);
+
+        // get spatial lane's snapshot's iterator based on the R2Shape, this returns all the points in the spatial lane
+        // within the circle
+        final Iterator<SpatialLane.Entry<Uri, R2Shape, Value>> newProximity = cityMapSnapshot.iterator(newCircle);
+        while (newProximity.hasNext()) {
+          final SpatialLane.Entry<Uri, R2Shape, Value> entry = newProximity.next();
+          newIntersections = newIntersections.updated(entry.getKey(), entry.getValue());
+          final Uri neighborUri = entry.getKey();
+          if (!neighborUri.equals(intersectionUri)) {
+            System.out.println(intersectionUri + " found intersection within approximately 0.25km radius-" + entry.getKey());
+          }
+        }
+      }
+    }
+  }
 
   public void linkIntersections() {
     if (intersectionsLink == null) {
